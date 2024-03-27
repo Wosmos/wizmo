c4301e2f980ef711fc9306d1eef526967b52693d
Wosmos <m.wasifmalik17@gmail.com>
2024-03-27 19:11:42 +0500

    - set

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <nav className='flex justify-between items-center w-full mb-10 pt-3  '>
         <span className='flex flex-col text-white mt-4'>
           <p className='-mb-8'>Powered by</p>
-          <a href='https://wasiff.vercel.app/' target='_blank' rel='noreferrer'>
+          <a href='https://wasiff.vercel.app/' target='_blank' rel='noreferrer' className='scale-x-105 hover:scale-110 transition-all ease-in-out duration-200 '>
             <Logo width='100' height='100' alt='sumz_logo' />
           </a>
         </span>
@@ -17,10 +17,10 @@ const Hero = () => {
         <Socials />
       </nav>
 
-      <section className='mt-0  md:-mt-12'>
-        <h1 className='md:text-9xl text-6xl font-extrabold -mb-8 font-inter text-center bg-gradient-to-b from-blue-600 to-cyan-600 bg-clip-text text-transparent  '>
+      <section className='  md:mt-8'>
+        {/* <h1 className='md:text-9xl text-6xl font-extrabold -mb-8 font-inter text-center bg-gradient-to-b from-blue-600 to-cyan-600 bg-clip-text text-transparent  '>
           Wizmo
-        </h1>
+        </h1> */}
         <h1 className='head_text -mb-2'>
           Unlock the <span className='blue_gradient'>Power </span> of{' '}
           <br className='max-md:hidden' />
